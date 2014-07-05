3c0ede4c0ced8b667265caf792710087d569ff70
daanielmarklund <daniel.m.widmark@gmail.com>
2014-07-05 22:13:21 +0200

    Commit of version 0.1
    Stable version of 0.1. Includes local storage

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php require "licensify.php"; $licensify = new licensify(); $licensify->validate(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Licensify PHP Library - Demo</title>    
+</head>
+
+<body id="page-top" class="index">
+    Test application contents running here.
+</body>
+
+</html>
